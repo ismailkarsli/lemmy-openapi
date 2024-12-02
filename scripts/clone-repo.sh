c47563e95e266f0a1176102357a24a02d7d0a346
İsmail Karslı <ismail@karsli.net>
2024-12-02 18:10:56 +0300

    refname:short

--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-URL=${URL:-"https://github.com/LemmyNet/lemmy-js-client/archive/refs/heads/main.zip"}
+URL=${URL:-"https://github.com/LemmyNet/lemmy-js-client.git"}
 
 if [ -d "lemmy-js-client"  ]; then
   rm -rf lemmy-js-client
 fi
 
-curl -L "$URL" -o lemmy-js-client.zip
-unzip lemmy-js-client.zip
-rm lemmy-js-client.zip
-mv lemmy-js-client-main lemmy-js-client
+git clone "$URL"
+cd lemmy-js-client || exit 1
+git fetch --all --tags
